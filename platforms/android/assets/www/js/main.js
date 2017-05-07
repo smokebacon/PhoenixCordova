1da3638bb41ac654c2279cbf7826883157eac521
Panjapol Chiemsombat <panjapol.chiemsombat@gmail.com>
2017-05-08 09:28:06 +1000

    added new jquery
    - not refer to yet but will tried to implement if need be

--- platforms/android/assets/www/js/main.js
+++ platforms/android/assets/www/js/main.js
@@ -14,6 +14,19 @@ var pageinited = false;
 /////////////////////////////////////////jquery On Document Ready
 $(document).on("pageinit", function(){
 
+        var rootURL="http://10.0.2.2/sites/PhoenixSlim/"
+
+        var CustomerID;
+        var Email;
+        var Password;
+        var Name;
+        var Middle;
+        var Last;
+        var StreetNo;
+        var StreetName;
+        var Suburb;
+        var Postcode;
+        var Phone;
 
 
         // Billk added code
@@ -22,7 +35,17 @@ $(document).on("pageinit", function(){
         
         // Homepage Event Handlers
         $("#homepage").live("pagebeforeshow", function(){
-//             alert("Before show homepage");
+
+        var auth;
+        var storage = window.localStorage;
+        var value = storage.getItem(auth);
+        if(auth == null)
+        {
+            alert("null auth");
+            jQuery.mobile.changePage("#register");
+        }
+
+             alert("Before show homepage");
         }); // end homepage live beforepageshow
 
           $("#homepage").live("pageaftershow", function(){
@@ -49,9 +72,95 @@ $(document).on("pageinit", function(){
      	$("#page1").live("pagebeforehide", function(){
 //             alert("Before hide page1");
         }); // end page1 live pagebeforehide
-		// End Page 1 Events Handlers		
-                                     
+		// End Page 1 Events Handlers
 
-             
+        $("#btnSubmit").on("click",function(){
+
+            alert('submit clicked');
+            var url = "customer/add/";
+            var c=[];
+            c.CustomerID = $("#txfCustomerID").val();
+            c.Email = $("#txfEmail").val();
+            c.Password = $("#txfPassword").val();
+            c.Name = $("#txfName").val();
+            c.Middle = $("#txfMiddle").val();
+            c.Last = $("#txfLast").val();
+            c.StreetNo = $("#txfStreetNo").val();
+            c.StreetName = $("#txfStreetName").val();
+            c.Suburb = $("#txfSuburb").val();
+            c.Postcode = $("#txfPost").val();
+            c.Phone = $("#txfPhone").val();
+
+            var settings = {
+              "async": true,
+              "crossDomain": true,
+              "url": rootURL+ "customer/add/",
+              "method": "POST",
+              "headers": {
+                "content-type": "application/json",
+                "cache-control": "no-cache",
+                "postman-token": "8cde29d8-2bf5-16e4-4370-d93b8b324bdf"
+              },
+              "processData": false,
+              "data": "{\"Customer_Id\":\"020304\",\r\n\t\t\"First_Name\":\"First\",\r\n\t\t\"Middle_Initial\":\"m\",\r\n\t\t\"Last_Name\":\"last\",\r\n\t\t\"Street_No\":\"Street\",\r\n\t\t\"Street_Name\":\"Name\",\r\n\t\t\"Suburb\":\"Sub\",\r\n\t\t\"Postcode\":\"1234\",\r\n\t\t\"Email\":\"What@.com\",\r\n\t\t\"Phone\":\"123423123\",\r\n\t\t\"Password\":\"pass\"}"
+            }
+
+            $.ajax(settings).done(function (response) {
+              alert('done');
+            })
+
+//            $.ajax({
+//                type: "POST",
+//                contentType: 'application/json',
+//                url: rootURL + '/customer/add',
+//                dataType:'json',
+//                data: registerJSON(c),
+//            })
+//            .done(function(data){
+//            localStorage.auth = data.AKEY;
+//            localStorage.email = data.email;
+//            jQuery.mobile.changePage('#homepage',{transition:"none"});
+//            })
+//            .fail(function(data){alert("registration failed");});
+
+//            var settings = {
+//              "async": true,
+//              "crossDomain": true,
+//              "url": rootURL + "customer/add/",
+//              "method": "POST",
+//              "headers": {
+//              "content-type": "application/json",
+//              "cache-control": "no-cache",
+//              },
+//              "data": registerJSON(c)
+//            }
+//
+//            $.ajax(settings).done(function (response) {
+//              alert(response);
+//            });
+//
+
+
+        });
+//
+//        function registerJSON(c){
+//                   return JSON.stringify({
+//                      "Customer_Id":c.CustomerID,
+//                      "First_Name":c.Name,
+//                      "Middle_Initial":c.Middle,
+//                      "Last_Name":c.Last,
+//                      "Street_No":c.StreetNo,
+//                      "Street_Name":c.StreetName,
+//                      "Suburb":c.Suburb,
+//                      "Postcode":c.Postcode,
+//                      "Email":c.Email,
+//                      "Phone":c.Phone,
+//                      "Password":c.Password,
+//                      });
+//                }
+
+
+
+
 });  // end document on pageinit
 ///////////////////////////////////////// END jquery On Document Ready
